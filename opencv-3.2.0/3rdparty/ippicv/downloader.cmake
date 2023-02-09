0e3d0d105e5d0fd0419672d310fa0d714f2970f0
ixihuan428 <1219499871@qq.com>
2023-02-09 14:00:38 +0800

    cme

--- opencv-3.2.0/3rdparty/ippicv/downloader.cmake
+++ opencv-3.2.0/3rdparty/ippicv/downloader.cmake
@@ -50,16 +50,47 @@ function(_icv_downloader)
 
   set(OPENCV_ICV_PACKAGE_ARCHIVE "${CMAKE_CURRENT_LIST_DIR}/downloads/${OPENCV_ICV_PLATFORM}/${OPENCV_ICV_PACKAGE_NAME}")
   get_filename_component(OPENCV_ICV_PACKAGE_ARCHIVE_DIR "${OPENCV_ICV_PACKAGE_ARCHIVE}" PATH)
-  
+  if(EXISTS "${OPENCV_ICV_PACKAGE_ARCHIVE}")
+    file(MD5 "${OPENCV_ICV_PACKAGE_ARCHIVE}" archive_md5)
+    #if(NOT archive_md5 STREQUAL OPENCV_ICV_PACKAGE_HASH)
+    #  message(WARNING "ICV: Local copy of ICV package has invalid MD5 hash: ${archive_md5} (expected: ${OPENCV_ICV_PACKAGE_HASH})")
+    #  file(REMOVE "${OPENCV_ICV_PACKAGE_ARCHIVE}")
+    #  file(REMOVE_RECURSE "${OPENCV_ICV_PACKAGE_ARCHIVE_DIR}")
+    #endif()
+  endif()
 
+  #if(NOT EXISTS "${OPENCV_ICV_PACKAGE_ARCHIVE}")
+  #  if(NOT DEFINED OPENCV_ICV_URL)
+  #    if(DEFINED ENV{OPENCV_ICV_URL})
+  #      set(OPENCV_ICV_URL $ENV{OPENCV_ICV_URL})
+  #    else()
+  #      set(OPENCV_ICV_URL "https://raw.githubusercontent.com/opencv/opencv_3rdparty/${IPPICV_BINARIES_COMMIT}/ippicv")
+  #    endif()
+  #  endif()
 
+  #  file(MAKE_DIRECTORY ${OPENCV_ICV_PACKAGE_ARCHIVE_DIR})
+  #  message(STATUS "ICV: Downloading ${OPENCV_ICV_PACKAGE_NAME}...")
+  #  file(DOWNLOAD "${OPENCV_ICV_URL}/${OPENCV_ICV_PACKAGE_NAME}" "${OPENCV_ICV_PACKAGE_ARCHIVE}"
+  #       TIMEOUT 600 STATUS __status
+  #       EXPECTED_MD5 ${OPENCV_ICV_PACKAGE_HASH})
+  #  if(NOT __status EQUAL 0)
+  #    message(FATAL_ERROR "ICV: Failed to download ICV package: ${OPENCV_ICV_PACKAGE_NAME}. Status=${__status}")
+  #  else()
+      # Don't remove this code, because EXPECTED_MD5 parameter doesn't fail "file(DOWNLOAD)" step
+      # on wrong hash
+  #    file(MD5 "${OPENCV_ICV_PACKAGE_ARCHIVE}" archive_md5)
+  #    if(NOT archive_md5 STREQUAL OPENCV_ICV_PACKAGE_HASH)
+  #      message(FATAL_ERROR "ICV: Downloaded copy of ICV package has invalid MD5 hash: ${archive_md5} (expected: ${OPENCV_ICV_PACKAGE_HASH})")
+  #    endif()
+  #  endif()
+  #endif()
 
-  #ocv_assert(EXISTS "${OPENCV_ICV_PACKAGE_ARCHIVE}")
+  ocv_assert(EXISTS "${OPENCV_ICV_PACKAGE_ARCHIVE}")
   #ocv_assert(NOT EXISTS "${OPENCV_ICV_UNPACK_PATH}")
   #file(MAKE_DIRECTORY ${OPENCV_ICV_UNPACK_PATH})
   #ocv_assert(EXISTS "${OPENCV_ICV_UNPACK_PATH}")
 
-  message(STATUS "ICV: Unpacking ${OPENCV_ICV_PACKAGE_NAME} to ${OPENCV_ICV_UNPACK_PATH}...")
+  #message(STATUS "ICV: Unpacking ${OPENCV_ICV_PACKAGE_NAME} to ${OPENCV_ICV_UNPACK_PATH}...")
   execute_process(COMMAND ${CMAKE_COMMAND} -E tar xz "${OPENCV_ICV_PACKAGE_ARCHIVE}"
                   WORKING_DIRECTORY "${OPENCV_ICV_UNPACK_PATH}"
                   RESULT_VARIABLE __result)
